6a6c2422a072a4e6fd7d477b4e389d375b8c1bb9
Andrei Statescu <27307694+AndreiRStatescu@users.noreply.github.com>
2025-11-23 08:44:03 +0200

    Add basic next frontend

--- backend/main.py
+++ backend/main.py
@@ -1,2 +1,14 @@
-print("Hello")
+from fastapi import FastAPI
+import uvicorn
+
+app = FastAPI()
+
+
+@app.get("/healthz")
+async def healthz():
+    return {"status": "ok"}
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
 
